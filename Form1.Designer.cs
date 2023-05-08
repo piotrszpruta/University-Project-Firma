72635566f8b1f624de15527fcb081ffcc90d9027
Piotr Szpruta <piotrszpruta@yahoo.com>
2023-05-08 20:46:21 +0200

    Add toolstrip items and textboxes for editing and inserting information

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,10 +29,37 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.textBoxTelefon = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxNazwisko = new System.Windows.Forms.TextBox();
+            this.textBoxImie = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.panel3 = new System.Windows.Forms.Panel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.sprzedażToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.sprzedażToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.listaToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.nowaFakturaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nowaFakturaToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.sprzedażToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.listaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nowyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.edycjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usunięcieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usuńFakturęToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pokażNieuregulowaneFakturyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
@@ -56,6 +83,70 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(800, 100);
             this.panel1.TabIndex = 0;
+            // 
+            // textBoxTelefon
+            // 
+            this.textBoxTelefon.Location = new System.Drawing.Point(171, 67);
+            this.textBoxTelefon.Name = "textBoxTelefon";
+            this.textBoxTelefon.Size = new System.Drawing.Size(128, 20);
+            this.textBoxTelefon.TabIndex = 7;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(171, 28);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(128, 20);
+            this.textBoxEmail.TabIndex = 6;
+            // 
+            // textBoxNazwisko
+            // 
+            this.textBoxNazwisko.Location = new System.Drawing.Point(11, 67);
+            this.textBoxNazwisko.Name = "textBoxNazwisko";
+            this.textBoxNazwisko.Size = new System.Drawing.Size(128, 20);
+            this.textBoxNazwisko.TabIndex = 5;
+            // 
+            // textBoxImie
+            // 
+            this.textBoxImie.Location = new System.Drawing.Point(10, 28);
+            this.textBoxImie.Name = "textBoxImie";
+            this.textBoxImie.Size = new System.Drawing.Size(128, 20);
+            this.textBoxImie.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(168, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Telefon";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(168, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nazwisko";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Imię";
             // 
             // panel2
             // 
@@ -94,6 +185,48 @@
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
             // 
+            // sprzedażToolStripMenuItem
+            // 
+            this.sprzedażToolStripMenuItem.Name = "sprzedażToolStripMenuItem";
+            this.sprzedażToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.sprzedażToolStripMenuItem.Text = "&Sprzedaż";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5,
+            this.toolStripMenuItem6});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(122, 20);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(67, 22);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(67, 22);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(67, 22);
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(67, 22);
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(67, 22);
+            // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -104,6 +237,91 @@
             this.menuStrip1.Size = new System.Drawing.Size(800, 24);
             this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
+            // 
+            // sprzedażToolStripMenuItem2
+            // 
+            this.sprzedażToolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listaToolStripMenuItem1,
+            this.nowaFakturaToolStripMenuItem,
+            this.nowaFakturaToolStripMenuItem1,
+            this.usuńFakturęToolStripMenuItem,
+            this.pokażNieuregulowaneFakturyToolStripMenuItem});
+            this.sprzedażToolStripMenuItem2.Name = "sprzedażToolStripMenuItem2";
+            this.sprzedażToolStripMenuItem2.Size = new System.Drawing.Size(65, 20);
+            this.sprzedażToolStripMenuItem2.Text = "&Sprzedaż";
+            // 
+            // listaToolStripMenuItem1
+            // 
+            this.listaToolStripMenuItem1.Name = "listaToolStripMenuItem1";
+            this.listaToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.listaToolStripMenuItem1.Text = "Lista";
+            this.listaToolStripMenuItem1.Click += new System.EventHandler(this.listaToolStripMenuItem1_Click);
+            // 
+            // nowaFakturaToolStripMenuItem
+            // 
+            this.nowaFakturaToolStripMenuItem.Name = "nowaFakturaToolStripMenuItem";
+            this.nowaFakturaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nowaFakturaToolStripMenuItem.Text = "Nowy kontrahent";
+            this.nowaFakturaToolStripMenuItem.Click += new System.EventHandler(this.nowaFakturaToolStripMenuItem_Click);
+            // 
+            // nowaFakturaToolStripMenuItem1
+            // 
+            this.nowaFakturaToolStripMenuItem1.Name = "nowaFakturaToolStripMenuItem1";
+            this.nowaFakturaToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.nowaFakturaToolStripMenuItem1.Text = "Nowa faktura";
+            this.nowaFakturaToolStripMenuItem1.Click += new System.EventHandler(this.nowaFakturaToolStripMenuItem1_Click);
+            // 
+            // sprzedażToolStripMenuItem1
+            // 
+            this.sprzedażToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listaToolStripMenuItem,
+            this.nowyToolStripMenuItem,
+            this.edycjaToolStripMenuItem,
+            this.usunięcieToolStripMenuItem});
+            this.sprzedażToolStripMenuItem1.Name = "sprzedażToolStripMenuItem1";
+            this.sprzedażToolStripMenuItem1.Size = new System.Drawing.Size(74, 20);
+            this.sprzedażToolStripMenuItem1.Text = "&Pracownik";
+            // 
+            // listaToolStripMenuItem
+            // 
+            this.listaToolStripMenuItem.Name = "listaToolStripMenuItem";
+            this.listaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.listaToolStripMenuItem.Text = "Lista";
+            this.listaToolStripMenuItem.Click += new System.EventHandler(this.listaToolStripMenuItem_Click);
+            // 
+            // nowyToolStripMenuItem
+            // 
+            this.nowyToolStripMenuItem.Name = "nowyToolStripMenuItem";
+            this.nowyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nowyToolStripMenuItem.Text = "Nowy";
+            this.nowyToolStripMenuItem.Click += new System.EventHandler(this.nowyToolStripMenuItem_Click);
+            // 
+            // edycjaToolStripMenuItem
+            // 
+            this.edycjaToolStripMenuItem.Name = "edycjaToolStripMenuItem";
+            this.edycjaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.edycjaToolStripMenuItem.Text = "Edycja";
+            this.edycjaToolStripMenuItem.Click += new System.EventHandler(this.edycjaToolStripMenuItem_Click);
+            // 
+            // usunięcieToolStripMenuItem
+            // 
+            this.usunięcieToolStripMenuItem.Name = "usunięcieToolStripMenuItem";
+            this.usunięcieToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.usunięcieToolStripMenuItem.Text = "Usunięcie";
+            this.usunięcieToolStripMenuItem.Click += new System.EventHandler(this.usunięcieToolStripMenuItem_Click);
+            // 
+            // usuńFakturęToolStripMenuItem
+            // 
+            this.usuńFakturęToolStripMenuItem.Name = "usuńFakturęToolStripMenuItem";
+            this.usuńFakturęToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.usuńFakturęToolStripMenuItem.Text = "Usuń fakturę";
+            // 
+            // pokażNieuregulowaneFakturyToolStripMenuItem
+            // 
+            this.pokażNieuregulowaneFakturyToolStripMenuItem.Name = "pokażNieuregulowaneFakturyToolStripMenuItem";
+            this.pokażNieuregulowaneFakturyToolStripMenuItem.Size = new System.Drawing.Size(233, 22);
+            this.pokażNieuregulowaneFakturyToolStripMenuItem.Text = "Pokaż nieuregulowane faktury";
+            this.pokażNieuregulowaneFakturyToolStripMenuItem.Click += new System.EventHandler(this.pokażNieuregulowaneFakturyToolStripMenuItem_Click);
             // 
             // Form1
             // 
@@ -136,7 +354,33 @@
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.ToolStripMenuItem sprzedażToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
         private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sprzedażToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem listaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nowyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem edycjaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usunięcieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sprzedażToolStripMenuItem2;
+        private System.Windows.Forms.TextBox textBoxImie;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxTelefon;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxNazwisko;
+        private System.Windows.Forms.ToolStripMenuItem listaToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem nowaFakturaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nowaFakturaToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem usuńFakturęToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pokażNieuregulowaneFakturyToolStripMenuItem;
     }
 }
 
