53df1fac8b15e0fdf34ccd66e2209bee47b26b83
Piotr Szpruta <piotrszpruta@yahoo.com>
2023-05-08 20:47:48 +0200

    Load data to first data grid from "Lista" toolstrip

--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Linq;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,31 @@ namespace Firma
 {
     public partial class Form1 : Form
     {
+        static DataContext bazaDanychFirma = new DataContext(connectionString);
+        static Table<Pracownik> listaPracownikow = bazaDanychFirma.GetTable<Pracownik>();
+        static Table<FakturaSprzedazy> listaFakturSprzedazy = bazaDanychFirma.GetTable<FakturaSprzedazy>();
+
+        static Table<Kontrahent> listaKontrahentow = bazaDanychFirma.GetTable<Kontrahent>();
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void listaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // pobieranie kolekcji
+            var lp = from Pracownik in listaPracownikow
+                     orderby Pracownik.Nazwisko
+                     select new
+                     {
+                         Pracownik.Id,
+                         Pracownik.Imie,
+                         Pracownik.Nazwisko,
+                         Pracownik.Telefon,
+                         Pracownik.Email
+                     };
+            dataGridView1.DataSource = lp;
+        }
+
     }
 }
